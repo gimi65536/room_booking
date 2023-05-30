2c9d5bb43a735617f57420aaa7a1c83a067eebfa
gimi65536 <gimi65536@hotmail.com>
2023-05-30 22:08:56 +0900

    Add Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	output: 'standalone',
 	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 		config.module.rules.push({
 			test: /\.json5$/i,
